baf009e5145e93b7111e8644b32f95d460017da7
David Navarro <david.navarro@linux.intel.com>
2013-08-09 11:58:03 +0200

    Bug fix: server select is better with a non-zero timeout.

--- server/lwm2mserver.c
+++ server/lwm2mserver.c
@@ -195,7 +195,7 @@ int main(int argc, char *argv[])
         FD_SET(socket, &readfds);
 
         tv.tv_usec = 0;
-        tv.tv_sec = 0;
+        tv.tv_sec = 1;
 
         result = select(FD_SETSIZE, &readfds, 0, 0, &tv);
 
