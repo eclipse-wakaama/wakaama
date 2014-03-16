af73830c7d2486c697c51af20b47481bf34ef320
domedambrosio <dome.dambrosio@gmail.com>
2014-03-16 21:58:13 +0200

    Fixed: path_location string was deallocated before being sent

--- core/er-coap-13/er-coap-13.c
+++ core/er-coap-13/er-coap-13.c
@@ -1091,7 +1091,7 @@ coap_set_header_location_path(void *packet, const char *path)
     coap_pkt->location_path_len = strlen(path);
   }
 
-  coap_pkt->location_path = path;
+  coap_pkt->location_path = strdup(path);
 
   SET_OPTION(coap_pkt, COAP_OPTION_LOCATION_PATH);
   return coap_pkt->location_path_len;
--- core/packet.c
+++ core/packet.c
@@ -267,6 +267,7 @@ void lwm2m_handle_packet(lwm2m_context_t * contextP,
 
                 coap_error_code = message_send(contextP, response, fromSessionH);
 
+                free(response->location_path);
                 lwm2m_free(response->payload);
                 response->payload = NULL;
                 response->payload_len = 0;
