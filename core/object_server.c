36d8941f75d43262ab06f6a2c5477ce31b4c5214
David Navarro <david.navarro@intel.com>
2014-04-17 11:17:34 +0200

    More of the LWM2M Server Object.
    Signed-off-by: David Navarro <david.navarro@intel.com>

--- core/object_server.c
+++ core/object_server.c
@@ -28,14 +28,90 @@ David Navarro <david.navarro@intel.com>
 
 */
 
+/*
+ *  Resources:
+ *
+ *          Name         | ID | Operations | Instances | Mandatory |  Type   |  Range  | Units |
+ *  Short ID             |  0 |     R      |  Single   |    Yes    | Integer | 1-65535 |       |
+ *  Lifetime             |  1 |    R/W     |  Single   |    Yes    | Integer |         |   s   |
+ *  Default Min Period   |  2 |    R/W     |  Single   |    No     | Integer |         |   s   |
+ *  Default Max Period   |  3 |    R/W     |  Single   |    No     | Integer |         |   s   |
+ *  Disable              |  4 |     E      |  Single   |    No     |         |         |       |
+ *  Disable Timeout      |  5 |    R/W     |  Single   |    No     | Integer |         |   s   |
+ *  Notification Storing |  6 |    R/W     |  Single   |    Yes    | String  |         |       |
+ *  Binding              |  7 |    R/W     |  Single   |    Yes    |         |         |       |
+ *  Registration Update  |  8 |     E      |  Single   |    Yes    |         |         |       |
+ *
+ */
+
+#ifdef LWM2M_CLIENT_MODE
+
 #include "internals.h"
+
+#define RESOURCE_SHORTID_ID     0
+#define RESOURCE_LIFETIME_ID    1
+#define RESOURCE_MINPERIOD_ID   2
+#define RESOURCE_MAXPERIOD_ID   3
+#define RESOURCE_DISABLE_ID     4
+#define RESOURCE_TIMEOUT_ID     5
+#define RESOURCE_STORING_ID     6
+#define RESOURCE_BINDING_ID     7
+#define RESOURCE_UPDATE_ID      8
+
 
 coap_status_t object_server_read(lwm2m_context_t * contextP,
                                  lwm2m_uri_t * uriP,
                                  char ** bufferP,
                                  int * lengthP)
 {
-    return COAP_501_NOT_IMPLEMENTED;
+    if (!LWM2M_URI_IS_SET_INSTANCE(uriP))
+    {
+        return COAP_501_NOT_IMPLEMENTED;
+    }
+    else
+    {
+        lwm2m_server_t * serverP;
+
+        serverP = (lwm2m_server_t *)lwm2m_list_find((lwm2m_list_t *)contextP->serverList, uriP->instanceId);
+        if (serverP == NULL) return COAP_404_NOT_FOUND;
+
+        if (!LWM2M_URI_IS_SET_RESOURCE(uriP))
+        {
+            return COAP_501_NOT_IMPLEMENTED;
+        }
+        else
+        {
+            switch (uriP->resourceId)
+            {
+            case RESOURCE_SHORTID_ID:
+                *lengthP = lwm2m_int32ToPlainText(serverP->shortID, bufferP);
+                if (0 != *lengthP)
+                {
+                    return COAP_205_CONTENT;
+                }
+                else
+                {
+                    return COAP_500_INTERNAL_SERVER_ERROR;
+                }
+                break;
+
+            case RESOURCE_LIFETIME_ID:
+                return COAP_501_NOT_IMPLEMENTED;
+            case RESOURCE_MINPERIOD_ID:
+                return COAP_404_NOT_FOUND;
+            case RESOURCE_MAXPERIOD_ID:
+                return COAP_404_NOT_FOUND;
+            case RESOURCE_TIMEOUT_ID:
+                return COAP_404_NOT_FOUND;
+            case RESOURCE_STORING_ID:
+                return COAP_501_NOT_IMPLEMENTED;
+            case RESOURCE_BINDING_ID:
+                return COAP_501_NOT_IMPLEMENTED;
+            default:
+                return COAP_405_METHOD_NOT_ALLOWED;
+            }
+        }
+    }
 }
 
 coap_status_t object_server_write(lwm2m_context_t * contextP,
@@ -43,7 +119,42 @@ coap_status_t object_server_write(lwm2m_context_t * contextP,
                                   char * buffer,
                                   int length)
 {
-    return COAP_501_NOT_IMPLEMENTED;
+    if (!LWM2M_URI_IS_SET_INSTANCE(uriP))
+    {
+        return COAP_501_NOT_IMPLEMENTED;
+    }
+    else
+    {
+        lwm2m_server_t * serverP;
+
+        serverP = (lwm2m_server_t *)lwm2m_list_find((lwm2m_list_t *)contextP->serverList, uriP->instanceId);
+        if (serverP == NULL) return COAP_404_NOT_FOUND;
+
+        if (!LWM2M_URI_IS_SET_RESOURCE(uriP))
+        {
+            return COAP_501_NOT_IMPLEMENTED;
+        }
+        else
+        {
+            switch (uriP->resourceId)
+            {
+            case RESOURCE_LIFETIME_ID:
+                return COAP_501_NOT_IMPLEMENTED;
+            case RESOURCE_MINPERIOD_ID:
+                return COAP_404_NOT_FOUND;
+            case RESOURCE_MAXPERIOD_ID:
+                return COAP_404_NOT_FOUND;
+            case RESOURCE_TIMEOUT_ID:
+                return COAP_404_NOT_FOUND;
+            case RESOURCE_STORING_ID:
+                return COAP_501_NOT_IMPLEMENTED;
+            case RESOURCE_BINDING_ID:
+                return COAP_501_NOT_IMPLEMENTED;
+            default:
+                return COAP_405_METHOD_NOT_ALLOWED;
+            }
+        }
+    }
 }
 
 coap_status_t object_server_execute(lwm2m_context_t * contextP,
@@ -51,7 +162,20 @@ coap_status_t object_server_execute(lwm2m_context_t * contextP,
                                     char * buffer,
                                     int length)
 {
-    return COAP_501_NOT_IMPLEMENTED;
+    lwm2m_server_t * serverP;
+
+    serverP = (lwm2m_server_t *)lwm2m_list_find((lwm2m_list_t *)contextP->serverList, uriP->instanceId);
+    if (serverP == NULL) return COAP_404_NOT_FOUND;
+
+    switch (uriP->resourceId)
+    {
+    case RESOURCE_DISABLE_ID:
+        return COAP_404_NOT_FOUND;
+    case RESOURCE_UPDATE_ID:
+        return COAP_501_NOT_IMPLEMENTED;
+    default:
+        return COAP_405_METHOD_NOT_ALLOWED;
+    }
 }
 
 coap_status_t object_server_create(lwm2m_context_t * contextP,
@@ -59,12 +183,22 @@ coap_status_t object_server_create(lwm2m_context_t * contextP,
                                    char * buffer,
                                    int length)
 {
+    lwm2m_server_t * serverP;
+
+    serverP = (lwm2m_server_t *)lwm2m_list_find((lwm2m_list_t *)contextP->serverList, uriP->instanceId);
+    if (serverP == NULL) return COAP_404_NOT_FOUND;
+
     return COAP_501_NOT_IMPLEMENTED;
 }
 
 coap_status_t object_server_delete(lwm2m_context_t * contextP,
                                    lwm2m_uri_t * uriP)
 {
+    lwm2m_server_t * serverP;
+
+    serverP = (lwm2m_server_t *)lwm2m_list_find((lwm2m_list_t *)contextP->serverList, uriP->instanceId);
+    if (serverP == NULL) return COAP_404_NOT_FOUND;
+
     return COAP_501_NOT_IMPLEMENTED;
 }
 
@@ -73,11 +207,23 @@ coap_status_t object_security_create(lwm2m_context_t * contextP,
                                      char * buffer,
                                      int length)
 {
+    lwm2m_server_t * serverP;
+
+    serverP = (lwm2m_server_t *)lwm2m_list_find((lwm2m_list_t *)contextP->serverList, uriP->instanceId);
+    if (serverP == NULL) return COAP_404_NOT_FOUND;
+
     return COAP_501_NOT_IMPLEMENTED;
 }
 
 coap_status_t object_security_delete(lwm2m_context_t * contextP,
                                      lwm2m_uri_t * uriP)
 {
+    lwm2m_server_t * serverP;
+
+    serverP = (lwm2m_server_t *)lwm2m_list_find((lwm2m_list_t *)contextP->serverList, uriP->instanceId);
+    if (serverP == NULL) return COAP_404_NOT_FOUND;
+
     return COAP_501_NOT_IMPLEMENTED;
 }
+
+#endif
