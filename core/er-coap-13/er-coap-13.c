48106e89c9237db82e572d6e589a5d6a208a0a36
David Navarro <david.navarro@intel.com>
2014-04-24 15:25:23 +0200

    Fix copyright notices
    Signed-off-by: David Navarro <david.navarro@intel.com>

--- core/er-coap-13/er-coap-13.c
+++ core/er-coap-13/er-coap-13.c
@@ -36,6 +36,21 @@
  *      Matthias Kovatsch <kovatsch@inf.ethz.ch>
  */
 
+/*******************************************************************************
+ *
+ * This file is made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * The Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *    David Navarro, Intel Corporation - Adapt to usage in liblwm2m
+ *
+ *******************************************************************************/
+
 #include <stdlib.h>
 
 #include <string.h>
