f5c6893a6764810599632d5987b6d81180e48ee3
Achim Kraus <achim.kraus@bosch-si.com>
2015-02-23 16:44:33 +0100

    Bugfix: set LWM2M_URI_FLAG_OBJECT_ID. Signed-off-by: Achim Kraus <achim.kraus@bosch-si.com>

--- core/uri.c
+++ core/uri.c
@@ -196,6 +196,7 @@ int lwm2m_stringToUri(const char * buffer,
     readNum = prv_parse_number(buffer, buffer_len, &head);
     if (readNum < 0 || readNum > LWM2M_MAX_ID) return 0;
     uriP->objectId = (uint16_t)readNum;
+    uriP->flag |= LWM2M_URI_FLAG_OBJECT_ID;
 
     if (buffer[head] == '/') head += 1;
     if (head >= buffer_len) return head;
