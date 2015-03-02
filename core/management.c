75777e3390df2514bbb88f718bb64f0870ce8d0c
Achim Kraus <achim.kraus@bosch-si.com>
2015-03-02 15:47:48 +0100

    Bugfix: security issue, when payload is set unconditional.
    Signed-off-by: Achim Kraus <achim.kraus@bosch-si.com>

--- core/management.c
+++ core/management.c
@@ -68,14 +68,20 @@ coap_status_t handle_dm_request(lwm2m_context_t * contextP,
             int length = 0;
 
             result = object_read(contextP, uriP, &buffer, &length);
-            coap_set_payload(response, buffer, length);
-            if (result == COAP_205_CONTENT)
+            if (COAP_205_CONTENT == result)
             {
-                coap_set_payload(response, buffer, length);
-                // lwm2m_handle_packet will free buffer
                 if (IS_OPTION(message, COAP_OPTION_OBSERVE))
                 {
                     result = handle_observe_request(contextP, uriP, fromSessionH, message, response);
+                }
+                if (COAP_205_CONTENT == result)
+                {
+                    coap_set_payload(response, buffer, length);
+                    // lwm2m_handle_packet will free buffer
+                }
+                else
+                {
+                    lwm2m_free(buffer);
                 }
             }
         }
