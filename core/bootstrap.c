47e3446735d75768aabc736d939f6b7a28801062
Julien Vermillard <jvermillard@sierrawireless.com>
2015-04-21 17:37:46 +0200

    Fix copyright

--- core/bootstrap.c
+++ core/bootstrap.c
@@ -1,6 +1,6 @@
 /*******************************************************************************
  *
- * Copyright (c) 2013, 2014 Intel Corporation and others.
+ * Copyright (c) 2015 Sierra Wireless and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * and Eclipse Distribution License v1.0 which accompany this distribution.
