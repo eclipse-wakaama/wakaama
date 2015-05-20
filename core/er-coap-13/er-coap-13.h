3e75833c6b5880cb4d9b38e66c3908792ed6e20d
David Navarro <david.navarro@intel.com>
2015-05-20 14:22:55 +0200

    Increase buffer size
    Signed-off-by: David Navarro <david.navarro@intel.com>

--- core/er-coap-13/er-coap-13.h
+++ core/er-coap-13/er-coap-13.h
@@ -53,7 +53,7 @@
  * Larger data must be handled by the resource and will be sent chunk-wise through a TCP stream or CoAP blocks.
  */
 #ifndef REST_MAX_CHUNK_SIZE
-#define REST_MAX_CHUNK_SIZE     128
+#define REST_MAX_CHUNK_SIZE     512
 #endif
 
 #define COAP_DEFAULT_MAX_AGE                 60
