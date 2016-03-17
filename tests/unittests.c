fc1b82a605c8a56afaf4fcf383350fbed794abd7
David Graeff <david.graeff@web.de>
2016-03-17 16:00:52 +0100

    Activate two new test cases
    Signed-off-by: David Graeff <david.graeff@web.de>

--- tests/unittests.c
+++ tests/unittests.c
@@ -46,6 +46,12 @@ int main()
    if (CUE_SUCCESS != create_uri_suit()) {
        goto exit;
    }
+   if (CUE_SUCCESS != create_convert_numbers_suit()) {
+       goto exit;
+   }
+   if (CUE_SUCCESS != create_tlv_json_suit()) {
+       goto exit;
+   }
 
    CU_basic_set_mode(CU_BRM_VERBOSE);
    CU_basic_run_tests();
