8045de5fbd43d9f828a40983b7342c48f04ac4a2
David Navarro <david.navarro@intel.com>
2016-04-07 22:45:37 +0200

    Remove platforms folder.
    Signed-off-by: David Navarro <david.navarro@intel.com>

--- examples/utils/utils.cmake
+++
@@ -1,8 +0,0 @@
-# Provides UTILS_SOURCES_DIR and UTILS_SOURCES variables
-
-set(UTILS_SOURCES_DIR ${CMAKE_CURRENT_LIST_DIR})
-
-set(UTILS_SOURCES 
-    ${UTILS_SOURCES_DIR}/commandline.c
-    ${UTILS_SOURCES_DIR}/commandline.h)
-
