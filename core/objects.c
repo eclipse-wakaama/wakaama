f1515b4c76de16be883e4a457219af3f1d1d746b
David Navarro <david.navarro@intel.com>
2016-06-28 11:01:05 +0200

    Fix for issue #181: An empty string is a valid return value for Read of a single value.
    Signed-off-by: David Navarro <david.navarro@intel.com>

--- core/objects.c
+++ core/objects.c
@@ -202,7 +202,16 @@ coap_status_t object_read(lwm2m_context_t * contextP,
     if (result == COAP_205_CONTENT)
     {
         *lengthP = lwm2m_data_serialize(uriP, size, dataP, formatP, bufferP);
-        if (*lengthP == 0) result = COAP_500_INTERNAL_SERVER_ERROR;
+        if (*lengthP == 0)
+        {
+            if (*formatP != LWM2M_CONTENT_TEXT
+                || size != 1
+                || dataP->type != LWM2M_TYPE_STRING
+                || dataP->value.asBuffer.length != 0)
+            {
+                result = COAP_500_INTERNAL_SERVER_ERROR;
+            }
+        }
     }
     lwm2m_data_free(size, dataP);
 
