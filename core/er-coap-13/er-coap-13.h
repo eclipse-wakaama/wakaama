b92ec14e64c1781050266b94420c17023a18acc8
David Navarro <david.navarro@intel.com>
2017-03-09 15:08:05 +0100

    Fix for #246: make sure enum coap_option_t is 2-bytes wide.
    Signed-off-by: David Navarro <david.navarro@intel.com>

--- core/er-coap-13/er-coap-13.h
+++ core/er-coap-13/er-coap-13.h
@@ -174,6 +174,7 @@ typedef enum {
   COAP_OPTION_BLOCK1 = 27,        /* 1-3 B */
   COAP_OPTION_SIZE = 28,          /* 0-4 B */
   COAP_OPTION_PROXY_URI = 35,     /* 1-270 B */
+  OPTION_MAX_VALUE = 0xFFFF
 } coap_option_t;
 
 /* CoAP Content-Types */
