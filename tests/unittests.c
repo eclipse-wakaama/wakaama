fec3a073c89070ffffd39970395cd679996172c4
Reto Schneider <code@reto-schneider.ch>
2021-04-05 14:46:59 +0200

    tests: Set exit code on failures
    With this commit, failing tests will break the build.
    
    Signed-off-by: Reto Schneider <code@reto-schneider.ch>

--- tests/unittests.c
+++ tests/unittests.c
@@ -79,6 +79,14 @@ int main()
 
    CU_basic_set_mode(CU_BRM_VERBOSE);
    CU_basic_run_tests();
+   CU_basic_show_failures(CU_get_failure_list());
+   printf("\n");
+
+   if (CU_get_number_of_tests_failed() > 0) {
+     return 1;
+   }
+   return 0;
+
 exit:
    CU_cleanup_registry();
    return CU_get_error();
