f65222d622b8234dde263e8a1885f95a5d15e121
Lukas Woodtli <lukas.woodtli@husqvarnagroup.com>
2022-11-16 13:03:56 +0100

    Fix and refactor tests for CoAP block-wise transfer
    The tests are more modular and more readable now.
    This fixes #511.

--- tests/block1tests.c
+++ tests/block1tests.c
@@ -21,62 +21,102 @@
 #include "internals.h"
 #include "liblwm2m.h"
 
+static const char *URI = "/1/2/3";
+static const uint16_t BLOCK_SIZE = 5;
 
-static void handle_12345(lwm2m_block_data_t ** blk1,
-                                  const char * uri) {
-    uint8_t *buffer = (uint8_t *)"12345";
-    size_t bsize;
-    uint8_t *resultBuffer = NULL;
+static uint8_t handle_12345(lwm2m_block_data_t **blk1, uint8_t **resultBuffer, size_t *resultLen) {
+    const char *buffer = "12345";
+    const size_t bufferLength = strlen(buffer);
+    const int BLOCK_NUM = 0;
+    const bool BLOCK_MORE = true;
 
-    uint8_t st = coap_block1_handler(blk1, uri, buffer, 5, 5, 0, true, &resultBuffer, &bsize);
-    CU_ASSERT_EQUAL(st, COAP_231_CONTINUE)
-    CU_ASSERT_PTR_NULL(resultBuffer)
+    return coap_block1_handler(blk1, URI, (const uint8_t *const)buffer, bufferLength, BLOCK_SIZE, BLOCK_NUM, BLOCK_MORE,
+                               resultBuffer, resultLen);
 }
 
-static void handle_67(lwm2m_block_data_t ** blk1,
-                                  const char * uri) {
-    uint8_t *buffer = (uint8_t *)"67";
-    size_t bsize;
-    uint8_t *resultBuffer = NULL;
+static uint8_t handle_67(lwm2m_block_data_t **blk1, uint8_t **resultBuffer, size_t *resultLen) {
+    const char *buffer = "67";
+    size_t bufferLength = strlen(buffer);
 
-    uint8_t st = coap_block1_handler(blk1, uri, buffer, 2, 5, 1, false, &resultBuffer, &bsize);
-    CU_ASSERT_EQUAL(st, NO_ERROR)
-    CU_ASSERT_PTR_NOT_NULL(resultBuffer)
-    CU_ASSERT_EQUAL(bsize, 7)
-    CU_ASSERT_NSTRING_EQUAL(resultBuffer, "1234567", 7)
+    const int BLOCK_NUM = 1;
+    bool BLOCK_MORE = false;
+    return coap_block1_handler(blk1, URI, (const uint8_t *const)buffer, bufferLength, BLOCK_SIZE, BLOCK_NUM, BLOCK_MORE,
+                               resultBuffer, resultLen);
 }
-
 
 static void test_block1_nominal(void)
 {
     lwm2m_block_data_t * blk1 = NULL;
 
-    handle_12345(&blk1, "/1/2/3");
-    handle_67(&blk1, "/1/2/3");
+    uint8_t *resultBuffer = NULL;
+    size_t resultLen = 0;
+    uint8_t status = handle_12345(&blk1, &resultBuffer, &resultLen);
+    CU_ASSERT_EQUAL(status, COAP_231_CONTINUE)
+    CU_ASSERT_PTR_NOT_NULL(blk1)
+    CU_ASSERT_PTR_NULL(resultBuffer)
+
+    resultBuffer = NULL;
+    resultLen = 0;
+    status = handle_67(&blk1, &resultBuffer, &resultLen);
+    CU_ASSERT_EQUAL(status, NO_ERROR)
+    CU_ASSERT_PTR_NOT_NULL(blk1)
+    CU_ASSERT_PTR_NOT_NULL(resultBuffer)
+    CU_ASSERT_EQUAL(resultLen, 7)
+    CU_ASSERT_NSTRING_EQUAL(resultBuffer, "1234567", 7)
 
     free_block_data(blk1);
 }
 
-// This test needs rework...
-/*
 static void test_block1_retransmit(void)
 {
     lwm2m_block_data_t * blk1 = NULL;
 
-    handle_12345(&blk1, "/1/2/3/");
-    handle_12345(&blk1, "/1/2/3/");
-    handle_67(&blk1, "/1/2/3/");
-    handle_67(&blk1, "/1/2/3/");
-    handle_67(&blk1, "/1/2/3/");
+    uint8_t *resultBuffer = NULL;
+    size_t resultLen = 0;
+
+    uint8_t status = handle_12345(&blk1, &resultBuffer, &resultLen);
+    CU_ASSERT_EQUAL(status, COAP_231_CONTINUE)
+    CU_ASSERT_PTR_NOT_NULL(blk1)
+    CU_ASSERT_PTR_NULL(resultBuffer)
+
+    resultBuffer = NULL;
+    resultLen = 0;
+    status = handle_12345(&blk1, &resultBuffer, &resultLen);
+    CU_ASSERT_EQUAL(status, COAP_231_CONTINUE)
+    CU_ASSERT_PTR_NULL(resultBuffer)
+
+    resultBuffer = NULL;
+    resultLen = 0;
+    status = handle_67(&blk1, &resultBuffer, &resultLen);
+    CU_ASSERT_EQUAL(status, NO_ERROR)
+    CU_ASSERT_PTR_NOT_NULL(blk1)
+    CU_ASSERT_PTR_NOT_NULL(resultBuffer)
+    CU_ASSERT_EQUAL(resultLen, 7)
+    CU_ASSERT_NSTRING_EQUAL(resultBuffer, "1234567", 7)
+
+    /* Resetting `resultBuffer` or `resultLen` here gives an error. */
+    status = handle_67(&blk1, &resultBuffer, &resultLen);
+    CU_ASSERT_EQUAL(status, COAP_RETRANSMISSION)
+    CU_ASSERT_PTR_NOT_NULL(blk1)
+    CU_ASSERT_PTR_NOT_NULL(resultBuffer)
+    CU_ASSERT_EQUAL(resultLen, 7)
+    CU_ASSERT_NSTRING_EQUAL(resultBuffer, "1234567", 7)
+
+    /* Resetting `resultBuffer` or `resultLen` here gives an error. */
+    status = handle_67(&blk1, &resultBuffer, &resultLen);
+    CU_ASSERT_EQUAL(status, COAP_RETRANSMISSION)
+    CU_ASSERT_PTR_NOT_NULL(blk1)
+    CU_ASSERT_PTR_NOT_NULL(resultBuffer)
+    CU_ASSERT_EQUAL(resultLen, 7)
+    CU_ASSERT_NSTRING_EQUAL(resultBuffer, "1234567", 7)
 
     free_block_data(blk1);
 }
-*/
 
 static struct TestTable table[] = {
-        { "test of test_block1_nominal()", test_block1_nominal },
-        //{ "test of test_block1_retransmit()", test_block1_retransmit },
-        { NULL, NULL },
+    {"test of test_block1_nominal()", test_block1_nominal},
+    {"test of test_block1_retransmit()", test_block1_retransmit},
+    {NULL, NULL},
 };
 
 CU_ErrorCode create_block1_suit() {
