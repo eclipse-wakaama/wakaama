2ab625fe1b887e61754f065896eab1896fdb5572
Lukas Woodtli <lukas.woodtli@husqvarnagroup.com>
2024-01-17 18:20:35 +0100

    logging: Allow the use of a custom log handler
    The custom handler function can be used to overwrite the defalut log
    message handler.
    It is used to format the message and to define where it is written to.
    Also filtering can be applied.

--- tests/helper/log_handler.c
+++ tests/helper/log_handler.c
@@ -1,6 +1,6 @@
 /*******************************************************************************
  *
- * Copyright (c) 2023 GARDENA GmbH
+ * Copyright (c) 2024 GARDENA GmbH
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
@@ -15,18 +15,39 @@
  *   Lukas Woodtli, GARDENA GmbH - Please refer to git log
  *
  *******************************************************************************/
-
+#include "log_handler.h"
 #include "internals.h"
-#include "liblwm2m.h"
+
+#include <unistd.h>
 
 #ifdef LWM2M_WITH_LOGS
 
-#ifndef LWM2M_LOG_CUSTOM_HANDLER
+/* Buffer for captured log message */
+#define CAPTURE_BUF_SIZE 300
+static char capture_buf[CAPTURE_BUF_SIZE];
+static size_t capture_buf_index = 0;
+
+#ifdef LWM2M_LOG_CUSTOM_HANDLER
 void lwm2m_log_handler(const lwm2m_logging_level_t level, const char *const msg, const char *const func, const int line,
                        const char *const file) {
     (void)file;
-    lwm2m_printf("%s - [%s:%d] %s" LWM2M_NEW_LINE, STR_LOGGING_LEVEL(level), func, line, msg);
+    (void)line;
+
+    if (capture_buf_index < CAPTURE_BUF_SIZE) {
+        capture_buf_index += snprintf(capture_buf + capture_buf_index, CAPTURE_BUF_SIZE - capture_buf_index,
+                                      "%s - [%s] %s" LWM2M_NEW_LINE, STR_LOGGING_LEVEL(level), func, msg);
+    }
 }
 #endif
 
-#endif /* LWM2M_WITH_LOGS */
+char *test_log_handler_get_captured_message(void) {
+    capture_buf[CAPTURE_BUF_SIZE - 1] = '\0'; // just to be safe
+    return capture_buf;
+}
+
+void test_log_handler_clear_captured_message(void) {
+    memset(capture_buf, '\0', CAPTURE_BUF_SIZE);
+    capture_buf_index = 0;
+}
+
+#endif
