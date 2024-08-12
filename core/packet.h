013a15342de0ce43662fd6053b602b5daa196beb
Lukas Woodtli <lukas.woodtli@husqvarnagroup.com>
2024-08-12 15:45:34 +0200

    Move declaration for 'packet' into own header
    This is more C like and leads to a better modularized project.

---
+++ core/packet.h
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2024 GARDENA GmbH
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v20.html
+ * The Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *   Lukas Woodtli, GARDENA GmbH - Please refer to git log
+ *
+ *******************************************************************************/
+#ifndef WAKAAMA_PACKET_H
+#define WAKAAMA_PACKET_H
+#include "internals.h"
+
+uint8_t message_send(lwm2m_context_t *contextP, coap_packet_t *message, void *sessionH);
+
+#endif /* WAKAAMA_PACKET_H */
