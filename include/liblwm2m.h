137364378e33379739d98bbfac5973970bffa049
Lukas Woodtli <lukas.woodtli@husqvarnagroup.com>
2024-09-02 15:15:38 +0200

    lwm2mserver: Clean up list functions
    Each function for list manipulations is wrapped in a macro. This makes
    the code more consistent.

--- include/liblwm2m.h
+++ include/liblwm2m.h
@@ -283,20 +283,22 @@ typedef struct _lwm2m_list_t
 
 // defined in list.c
 // Add 'node' to the list 'head' and return the new list
-lwm2m_list_t * lwm2m_list_add(lwm2m_list_t * head, lwm2m_list_t * node);
-// Return the node with ID 'id' from the list 'head' or NULL if not found
-lwm2m_list_t * lwm2m_list_find(lwm2m_list_t * head, uint16_t id);
-// Remove the node with ID 'id' from the list 'head' and return the new list
-lwm2m_list_t * lwm2m_list_remove(lwm2m_list_t * head, uint16_t id, lwm2m_list_t ** nodeP);
+lwm2m_list_t *lwm2m_list_add(lwm2m_list_t *head, lwm2m_list_t *node);
 // Return the lowest unused ID in the list 'head'. The ID 0 as return value is currently unused.
 uint16_t lwm2m_list_newId(lwm2m_list_t * head);
+// Remove the node with ID 'id' from the list 'head' and return the new list
+lwm2m_list_t *lwm2m_list_remove(lwm2m_list_t *head, uint16_t id, lwm2m_list_t **nodeP);
+// Return the node with ID 'id' from the list 'head' or NULL if not found
+lwm2m_list_t *lwm2m_list_find(lwm2m_list_t *head, uint16_t id);
+// Count the number of nodes in the list
 size_t lwm2m_list_count(const lwm2m_list_t *head);
 // Free a list. Do not use if nodes contain allocated pointers as it calls lwm2m_free on nodes only.
 // If the nodes of the list need to do more than just "free()" their instances, don't use lwm2m_list_free().
 void lwm2m_list_free(lwm2m_list_t * head);
 
-#define LWM2M_LIST_ADD(H,N) lwm2m_list_add((lwm2m_list_t *)H, (lwm2m_list_t *)N);
-#define LWM2M_LIST_RM(H,I,N) lwm2m_list_remove((lwm2m_list_t *)H, I, (lwm2m_list_t **)N);
+#define LWM2M_LIST_ADD(H, N) lwm2m_list_add((lwm2m_list_t *)H, (lwm2m_list_t *)N)
+#define LWM2M_LIST_NEW_ID(H) lwm2m_list_newId((lwm2m_list_t *)H)
+#define LWM2M_LIST_RM(H, I, N) lwm2m_list_remove((lwm2m_list_t *)H, I, (lwm2m_list_t **)N)
 #define LWM2M_LIST_FIND(H,I) lwm2m_list_find((lwm2m_list_t *)H, I)
 #define LWM2M_LIST_COUNT(H) lwm2m_list_count((lwm2m_list_t *)H)
 #define LWM2M_LIST_FREE(H) lwm2m_list_free((lwm2m_list_t *)H)
