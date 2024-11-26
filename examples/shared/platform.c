1f57c8ea54ac96b2a4be4574985f91b31278186f
Reto Schneider <reto.schneider@husqvarnagroup.com>
2024-11-27 00:48:45 +0100

    doc: Fix repository name
    While https://github.com/eclipse/wakaama redirects to
    https://github.com/eclipse-wakaama/wakaama, the later is what should be
    used nowadays.

--- examples/shared/platform.c
+++ examples/shared/platform.c
@@ -72,7 +72,7 @@ int lwm2m_seed(void) {
      * Return a seed for random number generation, the seed must be a
      * different number at every boot and unpredictable, time(NULL) may not be
      * a reliable source as a seed.
-     * See: https://github.com/eclipse/wakaama/pull/711
+     * See: https://github.com/eclipse-wakaama/wakaama/pull/711
      */
     return time(NULL);
 }
