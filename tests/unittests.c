e364781941b8262bd4a7a773a43af1b461bca168
Lukas Woodtli <lukas.woodtli@husqvarnagroup.com>
2025-03-18 12:02:18 +0100

    Allow bigger packets if no block transfer is active
    In CoAP it's possible that a packets payload is bigger than the block
    size.
    Usually the complete message size must fit into the configured max.
    packet size.
    In case of block-transfer the payload (without header and options) needs
    to fit into the block size.

--- tests/unittests.c
+++ tests/unittests.c
@@ -106,6 +106,10 @@ int main(void) {
    if (CUE_SUCCESS != create_logging_test_suit())
        goto exit;
 #endif
+#ifdef LWM2M_SERVER_MODE
+   if (CUE_SUCCESS != create_message_size_test_suit())
+       goto exit;
+#endif
 
    CU_basic_set_mode(CU_BRM_VERBOSE);
    CU_basic_run_tests();
