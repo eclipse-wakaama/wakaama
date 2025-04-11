d9f3866574ae6cae005a61cb9611a30905c8965e
ashishmac7 <ashishmac777@gmail.com>
2025-04-12 00:47:50 +0530

    Fix: Check memory allocation before memset operation
    - Moved the NULL check after memory allocation to occur before the memset call.
      This prevents potential undefined behavior if the allocation fails.
    - Ensures safe memory handling and avoids crashes due to dereferencing NULL.
    
    Signed-off-by: CMT-Ashish <ashish.macwan@comminent.com>
    Signed-off-by: ashishmac7 <ashishmac777@gmail.com>

--- core/registration.c
+++ core/registration.c
@@ -1658,8 +1658,8 @@ static lwm2m_client_object_t *prv_decodeRegisterPayload(uint8_t *payload, size_t
             if (objectP == NULL)
             {
                 objectP = (lwm2m_client_object_t *)lwm2m_malloc(sizeof(lwm2m_client_object_t));
-                memset(objectP, 0, sizeof(lwm2m_client_object_t));
                 if (objectP == NULL) goto error;
+                memset(objectP, 0, sizeof(lwm2m_client_object_t));
                 objectP->id = id;
                 objList = (lwm2m_client_object_t *)LWM2M_LIST_ADD(objList, objectP);
             }
@@ -1676,8 +1676,8 @@ static lwm2m_client_object_t *prv_decodeRegisterPayload(uint8_t *payload, size_t
                 if (instanceP == NULL)
                 {
                     instanceP = (lwm2m_list_t *)lwm2m_malloc(sizeof(lwm2m_list_t));
-                    memset(instanceP, 0, sizeof(lwm2m_list_t));
                     instanceP->id = instance;
+                    memset(instanceP, 0, sizeof(lwm2m_list_t));
                     objectP->instanceList = LWM2M_LIST_ADD(objectP->instanceList, instanceP);
                 }
             }
