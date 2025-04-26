41d95d75c4e15dbc0176fe16d6021110ddbb1aac
shan12138 <liuhao2495@outlook.com>
2025-04-26 22:22:38 +0800

    fix a memory leak bug about a registion update request without a reply

--- core/registration.c
+++ core/registration.c
@@ -857,10 +857,13 @@ static void prv_handleRegistrationUpdateReply(lwm2m_context_t * contextP,
             LOG_ARG_DBG("%d Registration update failed", dataP->server->shortID);
         }
     }
-    if (packet != NULL && packet->code != COAP_231_CONTINUE)
-    {
-        lwm2m_free(dataP->payload);
-        transaction_free_userData(contextP, transacP);
+    if (packet != NULL && packet->code == COAP_231_CONTINUE) {
+            ; // remain userdata
+    } else {
+        uint8_t *payload = dataP->payload;
+        if (transaction_free_userData(contextP, transacP)) {
+            lwm2m_free(payload);
+        }
     }
 }
 
